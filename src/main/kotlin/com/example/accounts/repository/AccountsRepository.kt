08e2bce7b9af436c41b28c290616d169a3fa02d2
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-01-09 13:27:59 -0300

    Repositórios criados

--- src/main/kotlin/com/example/accounts/repository/AccountsRepository.kt
+++ src/main/kotlin/com/example/accounts/repository/AccountsRepository.kt
@@ -1,9 +1,10 @@
 package com.example.accounts.repository
 
+import com.example.accounts.entity.Accounts
 import com.example.accounts.entity.Customer
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CustomerRepository:JpaRepository<Customer,Long>{
+interface AccountsRepository:JpaRepository<Accounts,Long>{
 }

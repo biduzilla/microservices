625db919823b262111fad3ae4485c95d8a0bf637
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-02-10 16:45:50 -0300

    application.yml

--- configserver/src/main/kotlin/com/example/configserver/ConfigserverApplication.kt
+++ configserver/src/main/kotlin/com/example/configserver/ConfigserverApplication.kt
@@ -6,8 +6,8 @@ import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
 @EnableConfigServer
-class ConfigServerApplication
+class ConfigserverApplication
 
 fun main(args: Array<String>) {
-	runApplication<ConfigServerApplication>(*args)
+	runApplication<ConfigserverApplication>(*args)
 }

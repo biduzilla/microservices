6cb24efdd43eb324c7d6259ea14a7bf260fe929a
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-05-08 17:06:15 -0300

    gateway project and application config gateway

---
+++ gatewayserver/src/main/kotlin/com/example/gatewayserver/GatewayserverApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gatewayserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayserverApplication>(*args)
+}

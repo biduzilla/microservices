2edbd0bd8acb6bd85faa1f5d952bfd82b73a3389
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-05-13 13:46:27 -0300

    corrigido routeConfig

--- gatewayserver/src/main/kotlin/com/example/gatewayserver/GatewayserverApplication.kt
+++ gatewayserver/src/main/kotlin/com/example/gatewayserver/GatewayserverApplication.kt
@@ -2,10 +2,48 @@ package com.example.gatewayserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import java.time.LocalDateTime
 
 @SpringBootApplication
-class GatewayserverApplication
+class GatewayserverApplication {
+    @Bean
+    fun eazyBankRouteConfig(routeLocatorBuilder: RouteLocatorBuilder): RouteLocator {
+        return routeLocatorBuilder.routes()
+            .route { p ->
+                p.path("/eazybank/accounts/**")
+                    .filters { f ->
+                        f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${'$'}{segment}")
+                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                    }
+                    .uri("lb://ACCOUNTS")
+            }
+            .route { p ->
+                p.path("/eazybank/loans/**")
+                    .filters { f ->
+                        f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${'$'}{segment}")
+                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                    }
+                    .uri("lb://LOANS")
+            }
+            .route { p ->
+                p.path("/eazybank/cards/**")
+                    .filters { f ->
+                        f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${'$'}{segment}")
+                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                    }
+                    .uri("lb://CARDS")
+            }
+            .build()
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<GatewayserverApplication>(*args)
+    runApplication<GatewayserverApplication>(*args)
 }
+
+
+
+
